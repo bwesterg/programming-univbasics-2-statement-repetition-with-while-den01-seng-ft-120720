846960d115908c06b39ee17fc4ae0052116bf864
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-13 21:02:59 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,5 @@
-magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
+count = 10
+while count >=1 do
+ puts count
+ count-=10
 end
